e56426b391617fed0d345943178a7f5452ddd739
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-01-14 22:11:20 +0330

    initial commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = { extends: ["@commitlint/config-conventional"] };
+/**
+ * Accepted types:
+ *
+ * build
+ * chore
+ * ci
+ * docs
+ * feat  // triggers a major release
+ * fix  // triggers a patch release
+ * perf
+ * refactor
+ * revert
+ * style
+ * test
+ */
